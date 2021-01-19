f4484d0bae5f617d9dbdcec985237b034b83dddb
Shasties <dmerryman@eitccorp.com>
2021-01-19 13:06:56 -0500

    initial commit

---
+++ bot.py
@@ -0,0 +1,23 @@
+# Third Party imports
+import wmi
+
+# Custom Imports
+from utils import catherineParser as cParser
+
+# Instantiation
+c = wmi.WMI()
+
+def launch_gui():
+	pass
+	# TODO
+	# List files under ./files/commands
+		# Allow User to select Commands to Load or Create New File
+
+def main():
+	pass
+	# TODO
+	# Check if Catherine Process is running
+		# Launch GUI
+
+if __name__ == "__main__":
+	main()
---
+++ utils/catherineParser.py
@@ -0,0 +1,21 @@
+# Third Party
+import json
+
+# Convert String Commands to Functions
+actionMapping = {
+	"Up": Move("Up"),
+}
+
+### Commands
+def Move(direction):
+	pass
+	# While Player Position has not updated
+		# Hold Direction
+
+### Interpreter
+def init(filePath):
+	pass 
+	# Read file
+	# Validate Macros
+	# Perform Commands
+		# Handle Conditionals
